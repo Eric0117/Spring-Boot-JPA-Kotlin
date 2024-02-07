0af5979ec6d9ee3cf75b221dc3573f8ca2fe759e
Eric <ksh117@kakao.com>
2024-02-07 14:38:01 +0900

    add created,modified date via AuditingEntityListener

--- src/main/kotlin/com/eric/jpa/domain/base/BaseEntity.kt
+++ src/main/kotlin/com/eric/jpa/domain/base/BaseEntity.kt
@@ -3,15 +3,30 @@ package com.eric.jpa.domain.base
 import com.fasterxml.uuid.Generators
 import jakarta.persistence.*
 import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.Persistable
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.io.Serializable
+import java.time.LocalDateTime
 import java.util.*
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity : Persistable<UUID> {
     @Id
     @Column(columnDefinition = "uuid")
     private val id: UUID = Generators.timeBasedGenerator().generate()
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var modifiedAt: LocalDateTime = LocalDateTime.now()
+        protected set
 
     @Transient
     private var _isNew = true
