6707c0d8c8545d6e927786613aecde1a6a09b423
Eric <ksh117@kakao.com>
2024-02-07 14:25:27 +0900

    add allopen plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,12 @@ plugins {
     kotlin("plugin.jpa") version "1.9.22"
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
 group = "com.eric"
 version = "0.0.1-SNAPSHOT"
 java {
